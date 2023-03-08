26c193f68060ca6e4e7c2026e3e3478527ed827e
Han <59479453+recta-rrae@users.noreply.github.com>
2023-03-08 23:09:26 +0700

    code cleaning after implementing db backup

--- controllers/functionAndProcedureController.js
+++ controllers/functionAndProcedureController.js
@@ -1,21 +1,32 @@
 const Sequelize = require('sequelize');
 const { execute } = require('@getvim/execute');
 const cron = require('node-cron');
+var mysqldump = require('mysqldump');
 
 var cp = require('child_process');
 
 exports.backupDB = async (req, res) => {
   
-  console.log(req);
+  console.log(req.query);
+
+  // today's date in case there is no filename
+  var today = new Date();
+  var dd = String(today.getDate()).padStart(2, '0');
+  var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+  var yyyy = today.getFullYear();
+  var hh = today.getHours();
+  today = dd+'-'+mm+'-'+yyyy+'_'+hh;
 
   let db = req.query.db;
   let table = req.query.table || 'all';
+  let filename = req.query.fn || `${today}_backup.sql`;
+  let path = req.query.path || `%USERPROFILE%/Downloads`;
 
   switch (db) {
-    case 'frontend':
+    case 'backend':
       db = 'sims_backend_1.0';
       break;
-    case 'backend':
+    case 'frontend':
       db = 'sims_auth';
       break; 
     default:
@@ -23,34 +34,67 @@ exports.backupDB = async (req, res) => {
 
   if (table == "all") {
 
-    // dump the result straight to a file
-    mysqldump({
-      connection: {
-          host: 'localhost',
-          user: 'root',
-          password: '',
-          database: db,
-      },
-      dumpToFile: `C:/SQL Backup/HistoryTableBackupTest.sql`,
-    });
+    try {
+
+      // dump the result straight to a file
+      mysqldump({
+        connection: {
+            host: 'localhost',
+            user: 'root',
+            password: '',
+            database: db,
+        },
+        dumpToFile: `${path}/${filename}`,
+      });
+
+      res.status(200).json({
+        status: 'success',
+        message: 'Database successfully backed up.'
+      });
+
+    } catch (err) {
+
+      res.status(400).json({
+        status: 'error',
+        message: err
+      });
+
+    }
+
 
   } else {
 
-    // dump the result straight to a file
-    mysqldump({
-      connection: {
-          host: 'localhost',
-          user: 'root',
-          password: '',
-          database: db,
-      },
-      dump: {
-        tables: [
-            table
-        ],
-      },
-      dumpToFile: `C:/SQL Backup/HistoryTableBackupTest.sql`,
-    });
+    try {
+
+      // dump the result straight to a file
+      mysqldump({
+        connection: {
+            host: 'localhost',
+            user: 'root',
+            password: '',
+            database: db,
+        },
+        dump: {
+          tables: [
+              table
+          ],
+        },
+        dumpToFile: `${path}/${filename}`,
+      });
+
+      res.status(200).json({
+        status: 'success',
+        message: 'Database successfully backed up.'
+      })
+
+    } catch (err) {
+
+      res.status(400).json({
+        status: 'error',
+        message: err
+      });
+
+    }
   }
 
   
